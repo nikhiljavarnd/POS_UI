10adc02ea81b0bee8f0eb29e955f22fd18ad75e4
nikhiljavarnd <nikhil.javarndcorp@gmail.com>
2021-12-28 10:40:27 -0500

    Header cleanup and css added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import { BrowserRouter } from "react-router-dom";
 import Router from "./route/router";
 import "./App.css";
 
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ffb80e",
+    },
+    secondary: {
+      main: "#982121",
+    },
+  },
+});
+
 function App(props) {
   return (
     <BrowserRouter>
-      <Router {...props} />
+      <ThemeProvider theme={theme}>
+        <Router {...props} />
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
