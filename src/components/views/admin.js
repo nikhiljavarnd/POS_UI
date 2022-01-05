ba23dd3c5884fbb90bf225486a32f597c7c548cb
Yasir <YasirFaruqui@gmail.com>
2022-01-05 16:19:23 -0500

    Updated Profile card

--- src/components/views/admin.js
+++ src/components/views/admin.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ToppingListing from "../toppings/toppingListing";
+import ProfileCard from "../ProfileCard/ProfileCard";
 
 function AdminDashboard(props) {
   return (
     <React.Fragment>
+      <ProfileCard />
       <ToppingListing />
     </React.Fragment>
   );
